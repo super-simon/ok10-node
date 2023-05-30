95fa71b477be66dbcf025cc693d52e10510d82a2
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-05-30 16:48:55 +0300

    node lesson 01 ht

--- app.js
+++ app.js
@@ -1,90 +1,32 @@
-//MODULES
-const { sayHello } = require("./helpers/sayHello.helper");
-
-sayHello();
-
-//GLOBAL VARIABLES
-console.log("FROM app");
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd());
-
-//PATH
 const path = require("path");
+const fs = require("node:fs/promises");
 
-const joinedPath = path.join(__dirname, "folder", "subfolder", "text.txt");
-console.log("joinedPath: ", joinedPath);
+const folders = ["folder01", "folder02", "folder03", "folder04", "folder05"];
+const files = ["file01", "file02", "file03", "file04", "file05"];
+const workPath = path.join(__dirname, "work_folder");
 
-const normalizedPath = path.normalize("///folder\\subfolder/text.txt");
-console.log("normalizedPath: ", normalizedPath);
+fs.mkdir(workPath, { recursive: true }).then(() => {
+  const foldersPromises = folders.map(async (folder) => {
+    await fs.mkdir(path.join(workPath, folder), { recursive: true });
+  });
 
-const resolvedPath = path.join("folder", "subfolder", "text.txt");
-console.log("resolvedPath: ", resolvedPath);
+  const filesPromises = files.map(async (file) => {
+    await fs.writeFile(path.join(workPath, file), file);
+  });
 
-//OS
-const os = require("os");
-console.log("cpus: ", os.cpus());
-console.log("arch: ", os.arch());
-const { exec } = require("child_process");
+  Promise.all([...foldersPromises, ...filesPromises])
+    .then(() => {
+      fs.readdir(workPath, { withFileTypes: true }).then((list) => {
+        list.map((item) => {
+          console.log(
+            item.name,
+            " is a ",
+            item.isFile() ? "file" : "directory"
+          );
+        });
+      });
 
-//FS
-const fs = require("fs");
-const text2Path = path.join(__dirname, "folder", "subfolder", "text2.txt");
-fs.writeFile(text2Path, "Hello from again!", (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
-});
-
-fs.readFile(text2Path, { encoding: "utf-8" }, (err, data) => {
-  if (!err) {
-    console.log(data);
-  }
-});
-
-fs.appendFile(text2Path, "asdfasfasdfsaf", (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
-});
-
-fs.truncate(text2Path, (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
-});
-
-fs.unlink(text2Path, (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
-});
-
-fs.readdir(
-  path.join(__dirname, "folder"),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) {
-      throw new Error(err.message);
-    }
-    files.forEach((file) => {
-      console.log(file.name, " is a ", file.isFile() ? "file" : "directory");
-    });
-  }
-);
-
-fs.mkdir(
-  path.join(__dirname, "folder", "folder4"),
-  { recursive: true },
-  (err) => {
-    if (err) {
-      throw new Error(err.message);
-    }
-  }
-);
-
-fs.rmdir(path.join(__dirname, "folder", "folder4"), (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
+      // fs.rm(workPath, { recursive: true });
+    })
+    .catch((err) => [console.log(err.message)]);
 });
