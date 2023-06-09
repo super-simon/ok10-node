17c96655f1c0083875ba5c56cdeb999f2bba4a30
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-06-09 13:49:46 +0300

    eslint changes

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,33 +1,33 @@
+import { ApiError } from "../errors";
 import { User } from "../models/User.model";
 import { userRepository } from "../repositories/user.repository";
 import { IUser, IUserWithoutPassword } from "../types/user.type";
-import { ApiError } from "../errors";
 
 class UserService {
   public async findAll(): Promise<IUserWithoutPassword[]> {
     try {
-      return User.find().select("-password");
+      return await User.find().select("-password");
     } catch (e) {
       throw new ApiError(e.message, e.status);
     }
   }
 
   public async create(data: IUser): Promise<IUser> {
-    return userRepository.create(data);
+    return await userRepository.create(data);
   }
 
   public async findById(id: string): Promise<IUser | null> {
-    return User.findById(id);
+    return await User.findById(id);
   }
 
   public async updateById(id: string, data: IUser): Promise<IUser | null> {
-    return User.findByIdAndUpdate(id, data, {
+    return await User.findByIdAndUpdate(id, data, {
       returnDocument: "after",
     });
   }
 
   public async deleteById(id: string): Promise<IUser | null> {
-    return User.findByIdAndDelete(id);
+    return await User.findByIdAndDelete(id);
   }
 }
 
