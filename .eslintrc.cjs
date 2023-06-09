ea91bc601fcd47f9200e3c166576fb25b2cb11d1
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-06-09 13:38:54 +0300

    eslint changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,48 @@
-/* eslint-env node */
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "simple-import-sort", "import"],
   extends: [
     "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "simple-import-sort"],
   root: true,
-  rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+  env: {
+    node: true,
+    jest: true,
   },
+  rules: {
+    "@typescript-eslint/return-await": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "req|res|next",
+      },
+    ],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "simple-import-sort/imports": "error",
+    "import/first": "error",
+    "import/newline-after-import": ["error", { count: 1 }],
+    "import/no-duplicates": "error",
+    "no-console": "warn",
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: false,
+      },
+    ],
+  },
+  ignorePatterns: [".eslintrc.js"],
 };
